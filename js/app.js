ea3150ff4b56d0a1946eec1fd04f86e436c3ba7d
Alex Solovey <alex@sd-design.tk>
2021-10-26 23:49:29 +0300

    Сделал для мульти элементов через data-id и data-placeholder

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
  console.log('%c Application is running', 'background: #222; color: #bada55; padding:15px')
 
- const item = document.querySelector('.item')
+ const items = document.querySelectorAll('.item')
  const placeholders = document.querySelectorAll('.placeholder')
+ let itemId = null
+ let idPlaceholder = null
 
-item.addEventListener('dragstart', dragStart)
-item.addEventListener('dragend', dragEnd)
+items.forEach((item)=>{
+	item.addEventListener('dragstart', dragStart)
+	item.addEventListener('dragend', dragEnd)
+
+
 
 for (placeholder of placeholders) {
 	placeholder.addEventListener('dragover', dragOver)
@@ -13,33 +18,49 @@ for (placeholder of placeholders) {
 	placeholder.addEventListener('drop', dragDrop)
 }
 
+})
+
+
 function dragStart(event){
-console.log('drag start', event.target)
-event.target.classList.add('hold')
-setTimeout(() => {
-	event.target.classList.add('d-none')
-}, 0);
+	//console.log('drag start', event.target)
+	event.target.classList.add('hold')
+	itemId = event.target.dataset.id
+	setTimeout(() => {
+		event.target.classList.add('d-none')
+	}, 0);
+	
+	}
+	
+	function dragEnd(event){
+		//console.log('drag end')
+		event.target.classList.remove('hold')
+		event.target.classList.remove('d-none')
+	}
+	
+	function dragOver(event){
+		event.preventDefault()
+	}
+	
+	function dragEnter(event){
+		event.preventDefault()
+		event.target.classList.add('hovered')
+		idPlaceholder = event.target.dataset.placeholder
+	}
+	
+	function dragleave(event){
+		event.target.classList.remove('hovered')
+	}
+	
 
-}
-
-function dragEnd(event){
-	console.log('drag end')
-	event.target.classList.remove('hold')
-	event.target.classList.remove('d-none')
-}
-
-function dragOver(event){
-	event.preventDefault()
-}
-
-function dragEnter(event){
-	event.target.classList.add('hovered')
-}
-
-function dragleave(event){
-	event.target.classList.remove('hovered')
-}
 function dragDrop(event){
+	let itemDrop = document.querySelector(`[data-id="${itemId}"]`)
+	
+	//console.log(itemDrop)
 	event.target.classList.remove('hovered')
-	event.target.append(item)
+	if(idPlaceholder != null && idPlaceholder != undefined){
+		let placeholderDrop = document.querySelector(`[data-placeholder="${idPlaceholder}"]`)
+		event.target.append(itemDrop)
+	}
+	
 }
+
